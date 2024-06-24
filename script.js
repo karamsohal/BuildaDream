12a187b2f191b18400f081df5122dbecfbe745ef
karamsohal <122296221+karamsohal@users.noreply.github.com>
2024-06-23 21:50:58 -0400

    updates

--- script.js
+++ script.js
@@ -21,3 +21,21 @@ document.addEventListener('DOMContentLoaded', function () {
     });
     });
     });
+
+    
+    document.addEventListener('DOMContentLoaded', function () {
+    const image1 = document.getElementById('image1');
+    const image2 = document.getElementById('image2');
+    let angle1 = 0;
+    let angle2 = 0;
+    const speed1 = 20;
+    const speed2 = 30; 
+    setInterval(function () {
+    angle1 += 1;
+    image1.style.transform = `translate(-30%, -30%) rotate(${angle1}deg)`;
+    }, speed1);
+    setInterval(function () {
+    angle2 += -1;
+    image2.style.transform = `translate(-70%, -70%) rotate(${angle2}deg)`;
+    }, speed2);
+    });
